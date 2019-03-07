edafd0a48245a037867948d32c31af9abf381e9e
Romuald <romu@ld.com>
2019-03-07 10:50:46 +0100

    hom et connection

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ConnexionFazamComponent } from './connexion-fazam/connexion-fazam';
+@NgModule({
+	declarations: [ConnexionFazamComponent],
+	imports: [],
+	exports: [ConnexionFazamComponent]
+})
+export class ComponentsModule {}
