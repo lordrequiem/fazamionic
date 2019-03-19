21520980a9220ef3c5497f784b5aacc32a045425
Asmedon <dvid.depuydt@gmail.com>
2019-03-19 12:47:57 +0100

    imageServices.ts

---
+++ src/services/imageServices.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+
+
+@Injectable()
+export default class RegisterService {
+    private apiUrl = 'http://localhost:8100';
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    sendImage(image: Object) {
+        return this.http
+           .post(this.apiUrl, image)
+            
+    };
+    
+
+
+
+
+}
