10466469bc050d5f324b32b42ea5b145b00c9de3
karima <karima@gmail.com>
2019-03-20 10:14:12 +0100

    "service"

---
+++ src/app/services/VideoApiService.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export default class VideoApiService {
+    private apiUrl = 'http://localhost:8888/videos';
+    
+    constructor(private http: HttpClient) {
+    }
+
+    findAll() {
+        return this.http.get(this.apiUrl);
+    }
+
+    findById(id: any) {
+    }
+
+    search(data: any) {
+        const params = new HttpParams({ fromObject: data });
+
+        const req = this.http.get(
+            this.apiUrl + '/search', { params }
+        );
+
+        return req;
+    }
+}
